61937ff33ddca55d4447e8a3f945061ab9812f34
ReoKuriyama <kuriyama.reo@moneyforward.co.jp>
2019-05-13 16:49:18 +0900

    要望カテゴリー別の要望を返すように

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,7 +1,7 @@
 class RequestsController < ApplicationController
   def index
-    requests = Request.all
-    json_string = RequestSerializer.new(requests).serialized_json
+    request_categories = RequestCategory.all
+    json_string = RequestCategorySerializer.new(request_categories ,{include: [:requests]}).serialized_json
     render json: json_string
   end
 end
