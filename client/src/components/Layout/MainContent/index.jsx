c1e47ad4b761c8afd703e512dd890c60cef28ebd
ReoKuriyama <kuriyama.reo@moneyforward.co.jp>
2019-05-15 16:27:49 +0900

    jsonの整形処理を一部変更しリクエストが成功したときのみに行う
    モーダルの読み込み

--- client/src/components/Layout/MainContent/index.jsx
+++ client/src/components/Layout/MainContent/index.jsx
@@ -1,37 +1,43 @@
 import React from 'react'
 import axios from 'axios';
 import RequestCategoryModal from '../../RequestCategoryModal/index'
+import NewRequestModal from '../../NewRequestModal/index'
 import './index.scss'
 
 class MainContent extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {categories: {data: [], included: []}};
+    this.state = {
+      categories: {data: [], included: []},
+      formatted_categories: []
+    };
   }
 
   componentDidMount() {
     axios.get('http://localhost:3001/requests')
     .then((results) => {
       this.setState({categories: results.data})
+
+      const info = this.state
+      const requestsSortByCategory = [];
+      info.categories.data.map(function(category) {
+        const requests = [];
+        category.relationships.requests.data.map(function(request) {
+          requests.push(info.categories.included.find(item => item.id === request.id));
+        });
+        requestsSortByCategory.push(requests);
+      });
+      this.setState({formatted_categories: requestsSortByCategory})
     })
   }
 
-
   render() {
-   const info = this.state
-    const requestsSortByCategory = [];
-    info.categories.data.map(function(category) {
-      const requests = [];
-      category.relationships.requests.data.map(function(request) {
-        requests.push(info.categories.included.find(item => item.id === request.id));
-      });
-      requestsSortByCategory.push(requests);
-    });
 
     return (
       <div className='mainContent'>
         {this.state.categories.data.map((requestCategory, i) =>
-         <RequestCategoryModal key={i} title={requestCategory.attributes.name} requests={requestsSortByCategory[i]} />)}
+         <RequestCategoryModal key={i} title={requestCategory.attributes.name} requests={this.state.formatted_categories[i]} />)}
+         <NewRequestModal />
       </div>
     );
   }
