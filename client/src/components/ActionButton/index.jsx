69546e60c73cc1e4a5ac5402f23e4175488fcda3
ReoKuriyama <kuriyama.reo@moneyforward.co.jp>
2019-05-15 11:47:39 +0900

    新規作成ボタン設置

---
+++ client/src/components/ActionButton/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './index.scss'
+
+class ActionButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.props.onNewModalChange(e.target.value);
+  }
+
+  render() {
+    return (
+      <div className='btn-circle-3d' onClick={this.handleClick}>新規作成</div>
+    );
+  }
+}
+
+
+export default ActionButton
