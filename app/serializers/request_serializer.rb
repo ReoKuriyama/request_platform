d0ff2f43ebb099770327c5a669b43ffe996263a3
ReoKuriyama <kuriyama.reo@moneyforward.co.jp>
2019-05-16 10:25:16 +0900

    返却値にidを追加

--- app/serializers/request_serializer.rb
+++ app/serializers/request_serializer.rb
@@ -2,7 +2,7 @@ class RequestSerializer
   include FastJsonapi::ObjectSerializer
   belongs_to :request_category
 
-  attributes :creator, :summary, :current_problem, :improvement_idea, :office_id, :estimate, :count, :status, :will_do
+  attributes :id, :creator, :summary, :current_problem, :improvement_idea, :office_id, :estimate, :count, :status, :will_do
 
   attributes :created_at do |object|
     object.created_at.strftime('%Y/%m/%d')
