5028ef97e2d199c323d2cf4cefdd0bebc6e08c6b
ReoKuriyama <kuriyama.reo@moneyforward.co.jp>
2019-05-16 17:53:19 +0900

    要望カウントupdate処理受付

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,7 +1,9 @@
 class RequestsController < ApplicationController
+  before_action :set_request, only: %i[update]
+
   def index
     request_categories = RequestCategory.all
-    json_string = RequestCategorySerializer.new(request_categories ,{include: [:requests]}).serialized_json
+    json_string = RequestCategorySerializer.new(request_categories, include: [:requests]).serialized_json
     render json: json_string
   end
 
@@ -10,18 +12,29 @@ class RequestsController < ApplicationController
     if request.save
       head :ok
     else
-      
+    end
+  end
+
+  def update
+    if @request.update(request_params)
+      head :ok
+    else
     end
   end
 
   private
+
+  def set_request
+    @request = Request.find(params[:id])
+  end
 
   def request_params
     params.require(:request).permit(
       :creator,
       :summary,
       :office_id,
-      :request_category_id
+      :request_category_id,
+      :count
     )
   end
 end
