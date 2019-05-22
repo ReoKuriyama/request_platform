48331c6b5bf9072eecd31a77e319b4d8c0604948
ReoKuriyama <kuriyama.reo@moneyforward.co.jp>
2019-05-22 16:06:18 +0900

    ticket取得処理

--- app/controllers/get_requests_controller.rb
+++ app/controllers/get_requests_controller.rb
@@ -1,6 +1,19 @@
-class GerRequestsController < ApplicationController
+class GetRequestsController < ApplicationController
+  # def index
+  #   request_json = 1
+  # end
 
-  def index
-    request_json = 1
+  def show
+    request_ticket_json = Api::Zendesk::Ticket.new.show(params[:id])[:ticket]
+    request_comments_json = Api::Zendesk::Comment.new.index(params[:id])[:comments]
+
+    joined_comments = JoinZendeskComment.call(comments: request_comments_json)
+
+    ticket = Ticket.new(subject: request_ticket_json[:subject], description: request_ticket_json[:description], all_comments: joined_comments, zendesk_ticket_id: request_ticket_json[:id]) 
+
+    if ticket.save
+      true
+    else
+    end
   end
 end
