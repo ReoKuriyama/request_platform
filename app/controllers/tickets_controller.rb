b5ed725d428d29b3b37659cbe59b24dd9fede715
ReoKuriyama <kuriyama.reo@moneyforward.co.jp>
2019-05-24 16:18:52 +0900

    ticket update処理

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,7 +1,30 @@
 class TicketsController < ApplicationController
+  before_action :set_ticket, only: %i[update]
+
   def index
     tickets = Ticket.unclassified_tickets
     json_string = TicketSerializer.new(tickets).serialized_json
     render json: json_string
+  end
+
+  def update
+    if @ticket.update(ticket_params)
+      request = Request.find(ticket_params[:request_id]).tap{|request| request.count += 1}
+      request.save
+      head :ok
+    else
+    end
+  end
+
+  private
+
+  def set_ticket
+    @ticket = Ticket.find(params[:id])
+  end
+
+  def ticket_params
+    params.require(:ticket).permit(
+      :request_id
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :requests
   resources :request_categories, only: %i[index]
   resources :get_requests, only: %i[show]
-  resources :tickets, only: %i[index]
+  resources :tickets, only: %i[index update]
 end
