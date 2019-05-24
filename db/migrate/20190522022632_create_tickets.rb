eb7a83be92a0d2f0e11180f0260bd16af6e1b36b
ReoKuriyama <kuriyama.reo@moneyforward.co.jp>
2019-05-24 16:19:05 +0900

    型の変更

--- db/migrate/20190522022632_create_tickets.rb
+++ db/migrate/20190522022632_create_tickets.rb
@@ -3,7 +3,7 @@ class CreateTickets < ActiveRecord::Migration[5.1]
     create_table :tickets do |t|
 
       t.string :subject
-      t.string :description # The first comment on the ticket
+      t.text :description # The first comment on the ticket
       t.text :all_comments
       t.integer :zendesk_ticket_id
       t.references :request
