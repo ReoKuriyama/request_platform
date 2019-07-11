0dfc249330780c45ad76be8c22b684d4bed694b6
ReoKuriyama <kuriyama.reo@moneyforward.co.jp>
2019-07-11 15:42:01 +0900

    jira経由での取得になったのでattributesの変更

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,16 +5,11 @@ class Ticket < ApplicationRecord
     validates :subject
     validates :description
     validates :zendesk_ticket_id
-    validates :all_comments
   end
 
   validates :zendesk_ticket_id, uniqueness: true
 
   def self.unclassified_tickets
     where(request_id: nil)
-  end
-
-  def all_text_info
-    subject + description + all_comments
   end
 end
--- db/migrate/20190522022632_create_tickets.rb
+++ db/migrate/20190522022632_create_tickets.rb
@@ -1,11 +1,12 @@
 class CreateTickets < ActiveRecord::Migration[5.1]
   def change
     create_table :tickets do |t|
-
       t.string :subject
-      t.text :description # The first comment on the ticket
-      t.text :all_comments
+      t.text :description
+      t.integer :office_id
+      t.integer :jira_issue_id
       t.integer :zendesk_ticket_id
+      t.string :zendesk_reporter
       t.references :request
 
       t.timestamps
